a1a6cbed07c9049b965fc2cdfda27d4a2bcc7da6
Ashimdevnath <ashimdevnath72611@gmail.com>
2026-07-28 20:42:57 +0530

    feat: add round1 through round4 columns to independent_bottle_entries table

---
+++ prisma/migrations/20260728_add_bottle_rounds/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "independent_bottle_entries" ADD COLUMN "round1" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "independent_bottle_entries" ADD COLUMN "round2" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "independent_bottle_entries" ADD COLUMN "round3" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "independent_bottle_entries" ADD COLUMN "round4" INTEGER NOT NULL DEFAULT 0;
